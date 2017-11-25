ccffbe2a57b9b74be40011dd989e2d1f3476cb20
Kirill Pavlov <k@p99.io>
2017-11-25 21:16:51 +0800

    ADD: margin and positioning to watermark

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ const defaultOptions = {
 };
 
 /**
+ * Get canvas object with image corresponding to src
+ */
+const getResourceCanvas = function getResourceCanvas(src) {
+  const img = window.document.createElement('img');
+  img.src = src;
+
+  const canvas = window.document.createElement('canvas');
+  canvas.height = img.height;
+  canvas.width = img.width;
+  canvas.getContext('2d').drawImage(img, 0, 0);
+
+  return canvas;
+};
+
+/**
  * Return watermark canvas object to be placed on top of the image.
  *
  * @param {Object} options - a configuration object.
@@ -21,20 +36,57 @@ const defaultOptions = {
  * @param {} options.textColor
  * @returns {Canvas} - a canvas object to be placed on top of the image.
  */
-const getWatertextCanvas = function getWatertextCanvas(options = {}) {
-  const opts = Object.assign({}, defaultOptions, options);
-
+const getWatermarkCanvas = function getWatermarkCanvas(options) {
   const canvas = window.document.createElement('canvas');
-  canvas.width = opts.textWidth;
-  canvas.height = 1.8 * opts.textSize;
+  canvas.width = options.textWidth;
+  canvas.height = 1.8 * options.textSize;
 
   const ctx = canvas.getContext('2d');
-  ctx.fillStyle = opts.backgroundColor;
+  ctx.fillStyle = options.backgroundColor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = opts.textColor;
-  ctx.font = `${opts.textSize}px Sans-serif`;
+  ctx.fillStyle = options.textColor;
+  ctx.font = `${options.textSize}px ${options.textFont}`;
   ctx.textAlign = 'center';
-  ctx.fillText(opts.text, canvas.width / 2, opts.textSize + 2);
+  ctx.fillText(options.text, canvas.width / 2, options.textSize + 2);
+
+  return canvas;
+};
+
+const applyWatermark = function applyWatermark(canvas, options) {
+  const watermarkCanvas = getWatermarkCanvas(options);
+
+  const ctx = canvas.getContext('2d');
+  // Move to canvas center.All of the following calculations are relative to it.
+  const w = canvas.width / 2;
+  const h = canvas.height / 2;
+  ctx.translate(w, h);
+
+  // Shift watermark image 50% to the left regardless canvas orientation.
+  const sx = -watermarkCanvas.width / 2;
+  let sy;
+
+  switch (options.position) {
+    case 'left': {
+      ctx.rotate(-Math.PI / 2);
+      sy = options.margin + options.margin >= 0 ? -w : w - watermarkCanvas.height;
+      break;
+    }
+    case 'right': {
+      ctx.rotate(Math.PI / 2);
+      sy = options.margin + options.margin >= 0 ? -w : w - watermarkCanvas.height;
+      break;
+    }
+    case 'top': {
+      sy = options.margin + options.margin >= 0 ? -h : h - watermarkCanvas.height;
+      break;
+    }
+    case 'bottom':
+      sy = -options.margin + options.margin >= 0 ? h - watermarkCanvas.height : -h;
+      break;
+    default:
+      throw new Error(`Unknown "position" option: "${options.position}"`);
+  }
+  ctx.drawImage(watermarkCanvas, sx, sy);
 
   return canvas;
 };
@@ -53,20 +105,7 @@ const getWatertextCanvas = function getWatertextCanvas(options = {}) {
  */
 export default function watertext(resource, options = {}) {
   const opts = Object.assign({}, defaultOptions, options);
-
-  // Create new image element based on the resource
-  const img = window.document.createElement('img');
-  img.src = resource;
-
-  const canvas = window.document.createElement('canvas');
-  canvas.height = img.height;
-  canvas.width = img.width;
-
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0);
-
-  ctx.drawImage(getWatertextCanvas(opts), 0, 0);
-
+  const canvas = applyWatermark(getResourceCanvas(resource), opts);
   const url = canvas.toDataURL();
   return url;
 }
